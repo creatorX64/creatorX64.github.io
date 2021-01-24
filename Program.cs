ad0a5ba5d28a4967bb0d180062d6f5a0e242a29e
Hakan Guclu <creatorX64@outlook.com>
2021-01-24 21:08:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace HakanGuclu.Web
+{
+	public class Program
+	{
+		public static async Task<int> Main(string[] args)
+		{
+			return await Bootstrapper.Factory
+				.CreateWeb(args)
+				.RunAsync();
+		}
+	}
+}
